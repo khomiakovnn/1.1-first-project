c1374241d9a6fc1753362c5e91667d644a23dd14
Nikolay Khomiakov <nikolay.khomiakov@gmail.com>
2022-12-02 15:53:49 +0300

    Pagi prepare

--- 2.1-databases/models_list_displaying/books/views.py
+++ 2.1-databases/models_list_displaying/books/views.py
@@ -1,7 +1,23 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
+
+from books.models import Book
 
 
 def books_view(request):
     template = 'books/books_list.html'
-    context = {}
+    books = [{"name": b.name, "author": b.author, "pub_date": b.pub_date} for b in Book.objects.all()]
+    context = {'books': books}
+    return render(request, template, context)
+
+
+def date_view(request, pub_date):
+    template = 'books/books_pagi.html'
+    CONTENT = [{"name": b.name, "author": b.author, "pub_date": b.pub_date} for b in Book.objects.all()]
+    paginator = Paginator(CONTENT)
+    page = paginator.get_page()
+    context = {'books': books,
+               'date': pub_date,
+               'page': page,
+    }
     return render(request, template, context)
