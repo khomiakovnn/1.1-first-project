54c1ce1b72edc8f126fb12eda0e478ebc685c34f
Nikolay Khomiakov <nikolay.khomiakov@gmail.com>
2022-12-02 11:52:19 +0300

    Final

--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -8,14 +8,30 @@ def index(request):
 
 
 def show_catalog(request):
+    sort = request.GET.get('sort')
     template = 'catalog.html'
-    phones_objects = Phone.objects.all()
-    phones = [{'name': p.name, 'slug': p.slug, 'image': p.image, 'price': p.price} for p in phones_objects]
+    if sort == "name":
+        phones = Phone.objects.order_by('name').values('name', 'slug', 'image', 'price')
+    elif sort == "min_price":
+        phones = Phone.objects.order_by('price').values('name', 'slug', 'image', 'price')
+    elif sort == "max_price":
+        phones = Phone.objects.order_by('-price').values('name', 'slug', 'image', 'price')
+    else:
+        phones = Phone.objects.values('name', 'slug', 'image', 'price')
     context = {'phones': phones}
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    phone_object = Phone.objects.get(slug=slug)
+    phone_item = {
+        'name': phone_object.name,
+        'slug': phone_object.slug,
+        'image': phone_object.image,
+        'price': phone_object.price,
+        'release_date': phone_object.release_date,
+        'lte_exists': phone_object.lte_exists,
+    }
+    context = {'phone': phone_item}
     return render(request, template, context)
