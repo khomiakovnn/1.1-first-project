280ea61eab4a55cdc0d2e41a45d8a322e4d3c68a
Nikolay Khomiakov <nikolay.khomiakov@gmail.com>
2022-12-06 15:32:24 +0300

    no Paginator pagin

--- 2.1-databases/models_list_displaying/books/views.py
+++ 2.1-databases/models_list_displaying/books/views.py
@@ -13,11 +13,18 @@ def books_view(request):
 
 def date_view(request, pub_date):
     template = 'books/books_pagi.html'
-    CONTENT = [{"name": b.name, "author": b.author, "pub_date": b.pub_date} for b in Book.objects.all()]
-    paginator = Paginator(CONTENT)
-    page = paginator.get_page()
+    dates = list(set([str(b.pub_date) for b in Book.objects.all()]))
+    dates.sort()
+    if dates.index(pub_date) == len(dates)-1:
+        date_before = dates[dates.index(pub_date)-1]
+        date_after = dates[0]
+    else:
+        date_before = dates[dates.index(pub_date)-1]
+        date_after = dates[dates.index(pub_date)+1]
+    books = [{"name": b.name, "author": b.author, "pub_date": b.pub_date} for b in Book.objects.filter(pub_date=pub_date)]
     context = {'books': books,
                'date': pub_date,
-               'page': page,
+               'date_before': date_before,
+               'date_after': date_after,
     }
     return render(request, template, context)
