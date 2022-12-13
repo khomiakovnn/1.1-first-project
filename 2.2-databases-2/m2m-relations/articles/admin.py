ee8252549e31f1d6fbeb0cd97ec89aeddfad74c3
Nikolay Khomiakov <nikolay.khomiakov@gmail.com>
2022-12-13 11:03:12 +0300

    Final

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,36 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+from .models import Article, ArticlesScopes, Tags
+
+
+class RelationshipInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        count_main = 0
+        tags_list = []
+        for form in self.forms:
+            if form.cleaned_data.get('is_main'):
+                count_main += 1
+            if count_main > 1:
+                raise ValidationError('Основной тег уже отмечен')
+            if form.cleaned_data.get('tag') in tags_list:
+                raise ValidationError('Такой тег уже есть')
+            elif form.cleaned_data.get('tag') is not None:
+                tags_list.append(form.cleaned_data.get('tag'))
+
+
+class ScopesInline(admin.TabularInline):
+    model = ArticlesScopes
+    extra = 3
+    formset = RelationshipInlineFormset
 
 
 @admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [ScopesInline, ]
+
+
+@admin.register(Tags)
 class ArticleAdmin(admin.ModelAdmin):
     pass
--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -1,12 +1,22 @@
 from django.db import models
 
 
-class Article(models.Model):
+class ArticlesScopes(models.Model):
+    article = models.ForeignKey('Article', on_delete=models.CASCADE, related_name='scopes')
+    tag = models.ForeignKey('Tags', on_delete=models.CASCADE, related_name='scopes')
+    is_main = models.BooleanField()
 
+    class Meta:
+        verbose_name = 'Тематика статьи'
+        verbose_name_plural = 'Тематика статей'
+        ordering = ['-is_main', 'tag', ]
+
+
+class Article(models.Model):
     title = models.CharField(max_length=256, verbose_name='Название')
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
-    image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    image = models.ImageField(null=True, blank=True, verbose_name='Изображение', )
 
     class Meta:
         verbose_name = 'Статья'
@@ -14,3 +24,14 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Tags(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Название')
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+
+    def __str__(self):
+        return self.name
