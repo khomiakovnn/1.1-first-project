24013bf7197b9b24759cdd4a218a5ff1bd3b47a5
Nikolay Khomiakov <nikolay.khomiakov@gmail.com>
2022-12-29 19:36:56 +0700

    Prepare

---
+++ 3.3-permissions/api_with_restrictions/requester.py
@@ -0,0 +1,8 @@
+import requests
+
+
+if __name__ == '__main__':
+    token_admin = 'a0a1eef5ee878028643618adfd096981829bb333'
+    token_user1 = '9c2caac175b0c0aeec3956bf3bf2aad0fedc6033'
+    token_user2 = '81171a1ba4da71a02d8b81f69fd05976cbc5d4f4'
+    href = 'http://127.0.0.1:8000/api/advertisements/'
