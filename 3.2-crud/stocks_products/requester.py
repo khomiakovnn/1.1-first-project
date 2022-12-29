60b53849d4b1972090eaf40735ff59f8eb0dfa63
Nikolay Khomiakov <nikolay.khomiakov@gmail.com>
2022-12-29 12:45:49 +0700

    Obligatory done

--- 3.2-crud/stocks_products/requester.py
+++ 3.2-crud/stocks_products/requester.py
@@ -1,10 +1,56 @@
 import requests
 
-href = 'http://localhost:8000/api/v1'
-param = {
-    'param1': 'param1',
-}
-response = requests.get(href, param)
 
-print(response.text)
-print(response.status_code)
+def create_prod(title, description):
+    href = 'http://localhost:8000/api/v1/products/'
+    param = {
+        "title": title,
+        "description": description
+    }
+    response = requests.post(href, param)
+
+    print(response.text)
+    print(response.status_code)
+
+
+def upd_prod(pk, title, description):
+    href = 'http://localhost:8000/api/v1/products/' + str(pk) + '/'
+    param = {}
+    if title is not None:
+        param['title'] = title
+    if description is not None:
+        param['description'] = description
+    response = requests.patch(href, param)
+
+    print(response.text)
+    print(response.status_code)
+
+
+def create_stock():
+    href = 'http://localhost:8000/api/v1/stocks/'
+    param = {
+        "address": "Товарная ул., 23",
+        "positions": [
+            {
+                "product": 1,
+                "quantity": 250,
+                "price": 120.50
+            },
+            {
+                "product": 2,
+                "quantity": 100,
+                "price": 180
+            }
+        ]
+    }
+    response = requests.post(href, param)
+
+    print(response.text)
+    print(response.status_code)
+
+
+if __name__ == '__main__':
+    pass
+    # create_prod(title="Картофель", description="брянский") # Тест POST продукта (CREATE)
+    # upd_prod(pk=3, title="Перец чили", description="Острый перец") # Тест PATCH продукта (UPDATE)
+    create_stock()  # Тест POST склада (CREATE)
