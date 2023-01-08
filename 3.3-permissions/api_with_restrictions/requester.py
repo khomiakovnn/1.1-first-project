167e8da00b660a3bd7033ec7984948ee6c636d05
Nikolay Khomiakov <nikolay.khomiakov@gmail.com>
2023-01-08 16:24:39 +0700

    Update 1

--- 3.3-permissions/api_with_restrictions/requester.py
+++ 3.3-permissions/api_with_restrictions/requester.py
@@ -32,12 +32,28 @@ def del_adv(token, adv_id):
     print(response.text)
     print(response.status_code)
 
+def update_adv(token, adv_id, title, description, status):
+    href = 'http://127.0.0.1:8000/api/advertisements/' + str(adv_id) + '/'
+    headers = {
+        'authorization': 'Token ' + token,
+    }
+    data = {
+        'title': title,
+        'description': description,
+        'status': status
+    }
+    response = requests.patch(href, data=data, headers=headers)
+    print(response.text)
+    print(response.status_code)
+
 
 if __name__ == '__main__':
     token_admin = 'a0a1eef5ee878028643618adfd096981829bb333'
     token_user1 = '9c2caac175b0c0aeec3956bf3bf2aad0fedc6033'
     token_user2 = '81171a1ba4da71a02d8b81f69fd05976cbc5d4f4'
+
     # Функции работы с объявлениями
     # make_adv(token_user1, 'XXXX', 'WWWWW')  # Создать объявление
-    show_adv(15)  # Показать объявление
+    # show_adv(15)  # Показать объявление
     # del_adv(token_user1, 10)  # Удалить объявление
+    # update_adv(token_user1, 11, 'Меняем', 'Закрываем', 'CLOSED')  # Изменить объявление
